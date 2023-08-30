abae4769eec2f90a29e231af4a8bc2e882513d3e
Szabolcs Bazil Papp <papp.szabolcs.bazil@gmail.com>
2023-08-30 23:40:10 +0200

    init spring boot subproject

---
+++ app/src/test/java/hu/aestallon/agenda/app/AppAgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.aestallon.agenda.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAgendaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
